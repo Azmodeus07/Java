9ca31becb20ccde16174c1584b0feb0d04f405ee
Hritik Yadav <hritiksinghyadav02@gmail.com>
2023-10-30 10:36:43 +0530

    done

--- Sorting/Bubble_sort.java
+++ Sorting/Bubble_sort.java
@@ -1,9 +1,34 @@
 package Sorting;
+
+import java.util.Scanner;
+
 public class Bubble_sort{
+    /**
+     * 
+     */
     public static void main()
     {
-        Scanner sc=new Scanner (System.in);
-        
+        Scanner sc =new Scanner (System.in);
+        System.out.println("Enter size");
+        int n =sc.nextInt();
+        int ar[]=new int[n];
+        for(int i=0;i<n;i++)
+            ar[i]=sc.nextInt();
+        //input taking
+        sc.close();
+        for(int i=0;i<n-1;i++) //for selecting a element
+        {
+            for(int j=0;j<n-i;j++)//for finding the largest element and sending  it to the last
+            {
+                if(ar[i]<ar[j])
+                {
+                    int temp=ar[i];
+                    ar[i]=ar[j];
+                    ar[j]=temp;
+                }
+            }
+        }//output
+        for (int i : ar)
+            System.out.println(i);
     }
-
 }
